8e079ac994ce1427a6f6e7b60658857536f3cb1c
artistsir <manju725430@gmail.com>
2025-11-29 14:18:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-OWNER_ID = 1946995626  # Your Telegram ID
+OWNER_ID = 8385462088  # Your Telegram ID
 PORT = int(os.environ.get("PORT", 8080))
 
 # User states for session generation
